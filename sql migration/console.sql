10596a135e706431ecf889df524f82120f1614f8
Sukhdeep <SukhdeepSingh1821@gmail.com>
2025-02-21 17:30:21 -0600

    Sql Part 2

--- sql migration/console.sql
+++ sql migration/console.sql
@@ -16,3 +16,39 @@ CREATE TABLE salary(
     actual_salary INT,
     FOREIGN KEY (stu_id) REFERENCES account(stu_id)
 );
+
+CREATE TABLE expected_expenses(
+    stu_id INT NOT NULL,
+    cat_id INT NOT NULL,
+    amount  INT NOT NULL,
+    FOREIGN KEY (cat_id) REFERENCES categories(cat_id),
+    FOREIGN KEY (stu_id) REFERENCES account(stu_id)
+);
+
+CREATE TABLE actual_expenses(
+    stu_id INT NOT NULL,
+    expense_date DATE NOT NULL,
+    amount INT NOT NULL,
+    cat_id INT NOT NULL,
+    FOREIGN KEY (cat_id) REFERENCES categories(cat_id),
+    FOREIGN KEY (stu_id) REFERENCES account(stu_id)
+);
+
+CREATE TABLE expenses_summary AS (SELECT expected_expenses.stu_id,
+                              SUM(expected_expenses.amount) exp_amount,
+                              SUM(actual_expenses.amount) actual_amount,
+                             (SUM(expected_expenses.amount) - SUM(actual_expenses.amount)) exp_difference
+                       FROM actual_expenses
+                                INNER JOIN
+                            expected_expenses
+                            ON actual_expenses.stu_id = expected_expenses.stu_id
+                       GROUP BY expected_expenses.stu_id);
+
+
+CREATE TABLE net AS (SELECT salary.stu_id ,
+       (SUM(salary.actual_salary) - SUM(actual_expenses.amount)) actual_net
+FROM salary
+    INNER JOIN
+    actual_expenses
+    ON salary.stu_id =  actual_expenses.stu_id
+GROUP BY salary.stu_id);

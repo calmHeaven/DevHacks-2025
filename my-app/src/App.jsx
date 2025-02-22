0368fa273e116227b7e8f121c2c0e43bf6af57ab
lumaniOvili <162659243+lumaniOvili@users.noreply.github.com>
2025-02-21 19:15:55 -0600

    I created the intro page which asks the user their series of questions

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -2,15 +2,16 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./HomePage";
 import Login from "./LoginPage";
-//import SignUp from "./signUP";
-
+import SignUp from "./SignUp";
+import Intro from "./Intro";
 function App() {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<HomePage />} />
                 <Route path="/login" element={<Login />} />
-                
+                <Route path="/signUp" element={<SignUp />} />
+                <Route path="/intro" element={<Intro />} />
             </Routes>
         </Router>
         
